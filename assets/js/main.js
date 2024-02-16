5a719b2d5a9aac90656ac7fa3725e891b0d4d19e
nicodellolio <nico.dellolio18@gmail.com>
2024-02-16 15:03:12 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,35 @@
 const test = 'It works!' //string
 console.log(test);
 
+//---------------------------------//
+
+// Esercise 1
+const number = 100; //number
+console.log(number);
+
+
+// Esercise 2
+const pI = 3.14; //number
+console.log(pI);
+
+// Esercise 2/B
+//const pI = 3.1416; //number
+//! Error. Cannot redeclare block-scoped variable 'pI'.
+
+// Excercise 2/C
+const radius = 8; //number
+
+let molt = radius * 2 * pI; //number
+
+const circle = molt; //variable
+console.log(circle)
+
+
+// Esercise 3
+let name = 'Carlo'; 
+
+name = 'Marco';
+console.log(name);
 
 
 
